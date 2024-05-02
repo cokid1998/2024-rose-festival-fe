fd5d54cb28974912f66eebfd15737f99ebc6f340
cokid1998 <cokid98@naver.com>
2024-05-03 00:02:18 +0900

    [Design : front] Header 구성
    - header에 icon button 추가

--- src/components/Base/Header/Header.jsx
+++ src/components/Base/Header/Header.jsx
@@ -1,5 +1,20 @@
+import {
+  HeaderWrapper,
+  IconMenuWrapper,
+} from "@src/components/Base/Header/Header.styled";
+import Menu from "@public/svg/Menu.svg";
+import Language from "@public/svg/Language.svg";
+import Image from "next/image";
+
 const Header = () => {
-  return <>Header</>;
+  return (
+    <HeaderWrapper>
+      <IconMenuWrapper>
+        <Image src={Menu} alt="Menu-icon" />
+        <Image src={Language} alt="Language-icon" />
+      </IconMenuWrapper>
+    </HeaderWrapper>
+  );
 };
 
 export default Header;
---
+++ src/components/Base/Header/Header.styled.jsx
@@ -0,0 +1,17 @@
+import styled from "@emotion/styled";
+
+export const HeaderWrapper = styled("header")`
+  width: 100%;
+  height: 70px;
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+`;
+
+export const IconMenuWrapper = styled("div")`
+  width: 310px;
+  height: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
