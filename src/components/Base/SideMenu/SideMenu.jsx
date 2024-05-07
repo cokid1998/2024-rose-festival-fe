e45821ac1ea7a4763d98a6f80cce262800aae62f
dbwjd <dbwjd3866@naver.com>
2024-05-07 21:57:32 +0900

    add sidemenu

--- src/components/Base/SideMenu/SideMenu.jsx
+++ src/components/Base/SideMenu/SideMenu.jsx
@@ -3,6 +3,9 @@ import { useEffect } from "react";
 import Close from "@public/svg/Close.svg";
 import Image from "next/image";
 import { AnimatePresence } from "framer-motion";
+import Language from "@public/svg/Language.svg";
+import { HeaderWrapper, IconMenuWrapper, HeaderLogo } from "../Header/Header.styled";
+import Link from "next/link";
 
 const SideMenu = ({ isMenuOpen, setIsMenuOpen }) => {
   const handleMenuOpen = () => {
@@ -31,8 +34,55 @@ const SideMenu = ({ isMenuOpen, setIsMenuOpen }) => {
               duration: 0.2,
             }}
           >
+            <HeaderWrapper>
+
+            <IconMenuWrapper>
             <Image src={Close} alt="Close-icon" onClick={handleMenuOpen} />
-            SideMenu
+            
+            <Link href={"/"}>
+            <HeaderLogo>조선대장미축제</HeaderLogo>
+            </Link>
+
+            <Image src={Language} alt="Language-icon" />
+            </IconMenuWrapper>
+
+            </HeaderWrapper>
+
+          <header style={{
+            width: "100%",
+            position: 'absolute',
+            top: '213px',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+          }}>
+
+        <Link href={"/"}>
+        <div style={{marginBottom: "20px"}}>메인화면</div>
+        </Link>
+
+        <Link href={"/notice"}>
+        <div style={{marginBottom: "20px"}}>공지사항</div>
+        </Link>
+
+        <Link href={"/notice"}>
+        <div style={{marginBottom: "20px"}}>오시는 길</div>
+        </Link>
+
+        <Link href={"/facilities"}>
+        <div style={{marginBottom: "20px"}}>편의시설 안내</div>
+        </Link>
+
+        <Link href={"/"}>
+        <div style={{marginBottom: "20px"}}>방명록</div>
+        </Link>
+
+        <Link href={"/"}>
+        <div style={{marginBottom: "20px"}}>만든이들</div>
+        </Link>
+          </header>
+            
+            
           </SideMenuWrapper>
         )}
       </AnimatePresence>
