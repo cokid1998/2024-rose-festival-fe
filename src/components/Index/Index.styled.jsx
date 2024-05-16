dc4ff604cba643a1983e117d02d91161f481847b
cokid1998 <cokid98@naver.com>
2024-05-16 21:59:40 +0900

    타이틀 css 롤백

--- src/components/Index/Index.styled.jsx
+++ src/components/Index/Index.styled.jsx
@@ -23,7 +23,6 @@ export const TitleWrapper = styled("h1")`
   font-size: 50px;
   color: white;
   line-height: 140%;
-  text-wrap: balance;
 `;
 export const PeriodWrapper = styled("div")`
   display: flex;
